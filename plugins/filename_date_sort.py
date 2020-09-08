4d6f99bb76286ee9dcb1ccec1e39e7d055b762aa
Shane Mulligan <mullikine@gmail.com>
2020-09-09 10:36:12 +1200

    Wed 05 Aug 2020 17:02:54 NZST - fix

--- plugins/filename_date_sort.py
+++ plugins/filename_date_sort.py
@@ -10,6 +10,8 @@ from random import random
 from ranger.container.directory import Directory
 
 from shanepy import *
+import re
+import functools
 
 # The function must return a number. The numbers are then compared
 # Directory.sort_dict['pathdate'] = lambda path: tv(path)
@@ -19,6 +21,9 @@ def extract_date(path):
     return b("rosie-extract-date", path)[0][:-1]
 
 def sort_by_pathdate(path):
+    extract_date(path)
+    gs = re.match("(\d+)\.(\d+)\.(\d+)", extract_date(path)).groups()
+    return gs[0] + 100 * gs[1] + 10000 * gs[2]
     # I can't do it this way because I need the entire list of paths.
     # Instead, I must use a hash function
     # b("sort -t . -n -k 3,3 -k 2,2 -k 1,1", b("ls /home/shane/notes/ws/tabs")[0])[0]
